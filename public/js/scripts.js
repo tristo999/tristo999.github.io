9de7fe46347d9c021219a2f0d63a0e8fbff6d229
tristo999 <tristanfitzwater@gmail.com>
2017-06-27 16:55:37 -0400

    Added Typing Text and Formatting

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,28 @@
-$(".element").typed({
-            strings: ["Are we partying yet?"],
-            typeSpeed: 0,
-cursorChar: " |",
+for(var n = 0; n < 30; n++) {
+	var number = n+32;
+	$(".intro-numbers").append("<p>1</p>");
+}
+$(function() {
+	$(".element").typeIt({
+	            strings: ["This is a test.", "a what?", "a test.", "a what?", "oh a test."],
+	            speed:100,
+		breakLines: false,
+			breakDelay: 100
+	});
+});
+$(function() {
+	$(".1").typeIt({
+	            strings: ["I am trying to write"],
+	            speed:100,
+		breakLines: false,
+			breakDelay: 100
+	});
+});
+$(function() {
+	$(".2").typeIt({
+	            strings: [" this in one sentance."],
+	            speed:100,
+		breakLines: false,
+			breakDelay: 100
+	});
 });
