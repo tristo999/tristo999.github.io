73fdb57edb8d9e001dcd490eb7ba41091ff8b00c
tristo999 <tristanfitzwater@gmail.com>
2017-06-28 14:16:13 -0400

    edits

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,7 @@
-for(var n = 0; n < 30; n++) {
-	var number = n+32;
-	$(".intro-numbers").append("<p>1</p>");
-}
+
 $(function() {
 	$(".element").typeIt({
-	            strings: ["This is a test.", "a what?", "a test.", "a what?", "oh a test."],
 	            speed:100,
-		breakLines: false,
-			breakDelay: 100
-	});
-});
-$(function() {
-	$(".1").typeIt({
-	            strings: ["I am trying to write"],
-	            speed:100,
-		breakLines: false,
-			breakDelay: 100
-	});
-});
-$(function() {
-	$(".2").typeIt({
-	            strings: [" this in one sentance."],
-	            speed:100,
-		breakLines: false,
-			breakDelay: 100
-	});
+	})
+	.tiType("I love typing.")
 });
