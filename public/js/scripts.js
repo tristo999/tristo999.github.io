fd8b073b5cf2b899b893d04dfb2c75e8d3dbb03c
tristo999 <tristanfitzwater@gmail.com>
2017-06-28 16:14:27 -0400

    Began implementing title screen animation.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,79 @@
-
+var speed = 5;
 $(function() {
-	$(".element").typeIt({
-	            speed:100,
+	$(".a1").typed({
+		strings: ["private"],
+		typeSpeed: speed,
+		cursorChar: "|",
+		callback: function() {
+			$(".typed-cursor").fadeOut("fast");
+			$(".a2").typed({
+				strings: [" void "],
+				typeSpeed: speed,
+				cursorChar: "|",
+				callback: function() {
+					$(".typed-cursor").fadeOut("fast");
+					$(".a3").typed({
+						strings: [" generateWebsite"],
+						typeSpeed: speed,
+						cursorChar: "|",
+						callback: function() {
+							$(".typed-cursor").fadeOut("fast");
+							$(".a4").typed({
+								strings: ["()"],
+								typeSpeed: speed,
+								cursorChar: "|",
+								callback: function() {
+									$(".typed-cursor").fadeOut("fast");
+									$(".a5").typed({
+										strings: ["{"],
+										typeSpeed: speed,
+										cursorChar: "|",
+										callback: function() {
+											$(".s1").typed({
+												strings: ["&nbsp;&nbsp;&nbsp;&nbsp;string"],
+												typeSpeed: speed,
+												cursorChar: "|",
+												callback: function() {
+													$(".typed-cursor").fadeOut("fast");
+													$(".s2").typed({
+														strings: [" name "],
+														typeSpeed: speed,
+														cursorChar: "|",
+														callback: function() {
+															$(".typed-cursor").fadeOut("fast");
+															$(".s3").typed({
+																strings: [" = "],
+																typeSpeed: speed,
+																cursorChar: "|",
+																callback: function() {
+																	$(".typed-cursor").fadeOut("fast");
+																	$(".s4").typed({
+																		strings: ["\"Tristan Fitzwater\""],
+																		typeSpeed: speed,
+																		cursorChar: "|",
+																		callback: function() {
+																			$(".typed-cursor").fadeOut("fast");
+																			$(".s5").typed({
+																				strings: [";"],
+																				typeSpeed: speed,
+																				cursorChar: "|"
+																			})
+																		}
+																	})
+																}
+															})
+														}
+													})
+												}
+											})
+										}
+									})
+								}
+							})
+						}
+					})
+				}
+			})
+		}
 	})
-	.tiType("I love typing.")
 });
